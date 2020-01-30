a14cfab561d45664eac46d8451604d9e97b961a1
OvinduArchanaNambukara <48377535+OvinduArchanaNambukara@users.noreply.github.com>
2020-01-30 19:27:59 +0530

    university management add to git hub

---
+++ university_management_system_/src/Main.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Home_Page home1=new Home_Page();
+        
+        
+        
+        
+        
+    }
+
+   
+}
